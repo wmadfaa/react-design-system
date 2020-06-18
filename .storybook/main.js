18e06602b38716d46ebb01796d11c69e844f91df
Wasim Almadfaa <wm.madfaa@gmail.com>
2020-06-18 10:28:42 +0200

    setup storybook

---
+++ .storybook/main.js
@@ -0,0 +1,9 @@
+module.exports = {
+  stories: ["../src/**/*.stories.js"],
+  addons: [
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-storysource",
+    "@storybook/addon-knobs",
+  ],
+};
--- src/Avatar.stories.js
+++ src/Avatar.stories.js
@@ -1,45 +1,26 @@
-import React from 'react';
+import React from "react";
+import { withKnobs, select, boolean } from "@storybook/addon-knobs";
 
-import { Avatar } from './Avatar';
+import { Avatar } from "./Avatar";
 
 export default {
-  title: 'Design System|Avatar',
-
+  title: "Design System|Avatar",
+  decorators: [withKnobs],
   parameters: {
     component: Avatar,
   },
 };
 
 export const standard = () => (
-  <Avatar
-    size="large"
-    username="Tom Coleman"
-    src="https://avatars2.githubusercontent.com/u/132554"
-  />
+  <Avatar size="large" username="Tom Coleman" src="https://avatars2.githubusercontent.com/u/132554" />
 );
 
 export const sizes = () => (
   <div>
-    <Avatar
-      size="large"
-      username="Tom Coleman"
-      src="https://avatars2.githubusercontent.com/u/132554"
-    />
-    <Avatar
-      size="medium"
-      username="Tom Coleman"
-      src="https://avatars2.githubusercontent.com/u/132554"
-    />
-    <Avatar
-      size="small"
-      username="Tom Coleman"
-      src="https://avatars2.githubusercontent.com/u/132554"
-    />
-    <Avatar
-      size="tiny"
-      username="Tom Coleman"
-      src="https://avatars2.githubusercontent.com/u/132554"
-    />
+    <Avatar size="large" username="Tom Coleman" src="https://avatars2.githubusercontent.com/u/132554" />
+    <Avatar size="medium" username="Tom Coleman" src="https://avatars2.githubusercontent.com/u/132554" />
+    <Avatar size="small" username="Tom Coleman" src="https://avatars2.githubusercontent.com/u/132554" />
+    <Avatar size="tiny" username="Tom Coleman" src="https://avatars2.githubusercontent.com/u/132554" />
   </div>
 );
 
@@ -65,10 +46,15 @@ export const large = () => (
   <div>
     <Avatar loading size="large" />
     <Avatar size="large" username="Tom Coleman" />
-    <Avatar
-      size="large"
-      username="Tom Coleman"
-      src="https://avatars2.githubusercontent.com/u/132554"
-    />
+    <Avatar size="large" username="Tom Coleman" src="https://avatars2.githubusercontent.com/u/132554" />
   </div>
+);
+
+export const knobs = () => (
+  <Avatar
+    loading={boolean("Loading")}
+    size={select("Size", ["tiny", "small", "medium", "large"])}
+    username="Dominic Nguyen"
+    src="https://avatars2.githubusercontent.com/u/263385"
+  />
 );
